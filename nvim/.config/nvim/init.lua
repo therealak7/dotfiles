c45c2c2f2c0632fc24fc968d83cadc556d50d591
Akash Soren <aksoren04@gmail.com>
2025-06-23 16:34:31 +0530

    configured init.lua added lazy manager

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,15 @@
+-- LAZY PACKAGE MANAGER --
+require("config.lazy")
+
+-- BASIC NEOVIM CONFIGS --
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+--vim.cmd("set relativenumber")
+vim.opt.number = true --dont know what this do
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "number" --dont know what this do
+
+-- REMAPS --
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true, silent = true }) -- remap jj to esc in insert mode

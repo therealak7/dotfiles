fe803e9cae713f1d35cd4800b61d783d7b0246d5
Akash Soren <aksoren04@gmail.com>
2025-06-28 14:01:21 +0530

    moved keymaps and options to respective files

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,16 +1,5 @@
-
--- BASIC NEOVIM CONFIGS --
---vim.cmd("set relativenumber")  > The vimsscript way [everything inside vim.cmd is a vimscript string]
---vim.opt.relativenumber = true  > The neovim lua way
+-- OPTIONS --
 require("config.options")
-
--- REMAPS --
-vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true, silent = true }) -- remap jj to esc in insert mode
-
--- SETTING LEADER KEY --
--- set to space inside config.lazy-
-vim.g.mapleader = " "
-vim.g.maplocalleader = "//"
 
 -- LAZY PACKAGE MANAGER --
 require("config.lazy")

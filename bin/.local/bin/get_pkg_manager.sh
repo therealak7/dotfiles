819e25a50157868ee72b1ec3db1531a4bf7ffcd7
Akash Soren <aksoren04@gmail.com>
2025-06-29 08:00:17 +0530

    added code to prevent execution as root

--- bin/.local/bin/get_pkg_manager.sh
+++ bin/.local/bin/get_pkg_manager.sh
@@ -1,15 +1,39 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Exit on error
 set -e
+
+for arg in "$@"; do
+    shift
+    case "$arg" in
+        "--root") override_root=1 ;;
+        *)
+        if echo "$arg" | grep -qv "^-"; then
+            tag="$arg"
+        else
+            echo "Invalid option $arg" >&2
+            exit 1
+        fi
+    esac
+done
+
+is_root() {
+    [ "$(id -u)" -ne 0 ]
+}
+
+if ! is_root && [ "${override_root:-0}" -eq 0 ]; then
+    echo "The script was ran under sudo or as root. The script will now exit"
+    echo "If you hadn't intended to do this, please execute the script without root access to avoid problems with spicetify"
+    echo "To override this behavior, pass the '--root' parameter to this script"
+    exit
+fi
+
 
 # Determine OS name
 os=$(uname)
 
 if [ "$os" = "Linux" ]; then
-    # echo "This is a Linux Machine"
 
-    # Initialize package manager
     pkg_manager=""
     distro_id=""
     distro_name=""
@@ -72,23 +96,44 @@ else
     exit 1
 fi
 
+# Installing Homebrew
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | sh
 
-# Adding repositories
-if [[ "$distro_id" == "fedora" || "$pkg_manager" == "dnf" ]]; then
-    echo "here fedora"
-elif [[ "$distro_id" == opensuse* || "$distro_id" == "suse" || "$pkg_manager" == "zypper" ]]; then
-    echo "opensuse"
-else
-    echo "Repositories not set for this distro"
-    echo "$distro_id"
-    echo "$distro_name"
-fi
+case "$distro_id" in
+    ubuntu|debian)
+        echo "Repositories not set"
+        ;;
+    rhel|centos|rocky|almalinux)
+        echo "Repositories not set"
+        ;;
+    fedora)
+        # Adding repositories
+        flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+        sudo dnf copr enable atim/lazygit -y
+        sudo dnf copr enable wezfurlong/wezterm-nightly -y
+        sudo dnf copr enable alternateved/eza -y
+        # TODO: shift this repository to gnome section
+        # sudo dnf copr enable monkeygold/nautilus-open-any-terminal -y # Gnome only
+        ;;
+    arch)
+        echo "Repositories not set"
+        ;;
+    alpine)
+        echo "Repositories not set"
+        ;;
+    suse|opensuse*)
+        # Adding repositories
+        flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+        sudo zypper ar https://download.opensuse.org/repositories/devel:/languages:/go/openSUSE_Factory/devel:languages:go.repo # repo for lazygit
+        brew install lolcat
+        ;;
+    *)
+        ;;
+esac
+
 
 # Installing with package manager
-# sudo $pkg_manager install -y figlet nvim ripgrep fd-find python3-pip eza lolcat lazygit wezterm fastfetch fzf stow fish git git-lfs brave librewolf wmctrl
-#
-# # Installing flatpaks
-# flatpak install flathub app.zen_browser.zen com.github.tenderowl.frog com.valvesoftware.Steam dev.vencord.Vesktop eu.betterbird.Betterbird com.stremio.Stremio
-#
-# # Install Mega - Online cloud storage service
-# wget https://mega.nz/linux/repo/Fedora_42/x86_64/megasync-Fedora_42.x86_64.rpm && sudo dnf install "$PWD/megasync-Fedora_42.x86_64.rpm"
+sudo $pkg_manager install -y figlet nvim ripgrep fd-find python3-pip eza lolcat lazygit wezterm fastfetch fzf stow fish git git-lfs
+
+# Installing flatpaks
+flatpak install flathub com.github.tenderowl.frog com.valvesoftware.Steam dev.vencord.Vesktop eu.betterbird.Betterbird com.stremio.Stremio io.gitlab.librewolf-community com.brave.Browser
